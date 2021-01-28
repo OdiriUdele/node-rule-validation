0b7c665ce20237e08ddfc8385da2784b8208c010
odiri udele <udele.odiri@techvibesltd.com>
2021-01-28 23:55:45 +0100

    field checks and rule validation created

--- src/app.js
+++ src/app.js
@@ -6,11 +6,11 @@ const handleError = require('./error');
 app.use(express.json())
 
 // routes
-app.post('/validate-rule',RuleDataExists);
+app.post('/validate-rule',RuleDataExists); //initial middleware to check if json value has the required fields
 
 app.use(require('./route/route'));
 
-app.use((err,req, res, next) => {
+app.use((err,req, res, next) => {//erroor handling
     if(err) {
       error = handleError(err);
       res.status(400).json(error)
