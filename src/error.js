42949c9cf53df51753d3c8b569aad13387a062f2
odiri udele <udele.odiri@techvibesltd.com>
2021-01-29 00:47:25 +0100

    pushing to heroku

--- src/error.js
+++ src/error.js
@@ -1,14 +1,14 @@
-const {retun_json} = require('./controller/Controller');
+var authController = require('./controller/Controller');
 module.exports = (err)=>{
     if(err instanceof SyntaxError && err.type=="entity.parse.failed"){
         if(err.message.includes('JSON')){
-        errorMessage = retun_json('Invalid JSON payload passed',null,'error');
+        errorMessage = authController.retun_json('Invalid JSON payload passed',null,'error');
         return errorMessage;
         }
     }
     else{
         console.log(err);
-        errorMessage = retun_json(err.message,null,'error');
+        errorMessage = authController.retun_json(err.message,null,'error');
         return errorMessage;
     }
     
